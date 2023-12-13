cd2c254ff0cb155cf4a934e18f1e3a56b943c368
Aris Lacdao <aceraven777@gmail.com>
2023-12-13 13:46:11 +0800

    Episode 11 - Express Router & MVC

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
+const blogRoutes = require('./routes/blogRoutes');
 
 // require environment variables
 require('dotenv').config();
@@ -31,57 +31,7 @@ app.get('/about', (req, res) => {
 });
 
 // blog routes
-app.get('/blogs', (req, res) => {
-    Blog.find().sort({ createdAt: -1 })
-        .then(result => {
-            res.render('index', { title: 'All Blogs', blogs: result });
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
-
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create a new Blog' });
-});
-
-app.post('/blogs', (req, res) => {
-    const blog = new Blog(req.body);
-
-    blog.save()
-        .then(result => {
-            res.redirect('/blogs');
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
-
-app.get('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-
-    Blog.findById(id)
-        .then(result => {
-            res.render('details', { title: 'Blog Details', blog: result });
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
-
-app.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-
-    Blog.findByIdAndDelete(id)
-        .then(result => {
-            res.json({
-                redirect: '/blogs',
-            });
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
+app.use('/blogs', blogRoutes);
 
 // 404 page
 app.use((req, res) => {
