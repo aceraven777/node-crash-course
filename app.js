81a76eb360b57ab20bb68f79de16bbd807cce802
Aris Lacdao <aceraven777@gmail.com>
2023-12-13 12:32:34 +0800

    Episode 10 - Get, Post & Delete Requests

--- app.js
+++ app.js
@@ -11,52 +11,16 @@ const app = express();
 
 // connect to mongodb
 mongoose.connect(process.env.DB_URI)
-    .then((result) => app.listen(3000))
-    .catch((err) => console.log(err));
+    .then(result => app.listen(3000))
+    .catch(err => console.log(err));
 
 // register view engine
 app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
-
-// mongoose and mongo sandbox routes
-app.get('/add-blog', (req, res) => {
-    const blog = new Blog({
-        title: 'new blog 2',
-        snippet: 'about my new blog',
-        body: 'more about my new blog',
-    });
-
-    blog.save()
-        .then((result) => {
-            res.send(result);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-
-app.get('/all-blogs', (req, res) => {
-    Blog.find()
-        .then((result) => {
-            res.send(result);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-
-app.get('/single-blog', (req, res) => {
-    Blog.findById('6577d3ff3b5ea6332a3030ef')
-        .then((result) => {
-            res.send(result);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
 
 app.get('/', (req, res) => {
     res.redirect('/blogs');
@@ -69,16 +33,54 @@ app.get('/about', (req, res) => {
 // blog routes
 app.get('/blogs', (req, res) => {
     Blog.find().sort({ createdAt: -1 })
-        .then((result) => {
+        .then(result => {
             res.render('index', { title: 'All Blogs', blogs: result });
         })
-        .catch((err) => {
+        .catch(err => {
             console.log(err);
         });
 });
 
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create a new Blog' });
+});
+
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body);
+
+    blog.save()
+        .then(result => {
+            res.redirect('/blogs');
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
+
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+
+    Blog.findById(id)
+        .then(result => {
+            res.render('details', { title: 'Blog Details', blog: result });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
+
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+
+    Blog.findByIdAndDelete(id)
+        .then(result => {
+            res.json({
+                redirect: '/blogs',
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
 });
 
 // 404 page
